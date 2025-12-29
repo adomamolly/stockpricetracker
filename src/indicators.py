f000e802dcebae4ded3823ab5ea3223121d44194
adwoa adoma <adomamolly@gmail.com>
2025-12-29 00:35:05 -0500

    Create indicators.py

---
+++ src/indicators.py
@@ -0,0 +1,9 @@
+import ta  # technical analysis indicators
+
+# part 3: function for adding technical indicators##
+
+
+def add_technical_indicators(data):
+    data['SMA_20'] = ta.trend.sma_indicator(data['Close'], window=20)
+    data['EMA_20'] = ta.trend.ema_indicator(data['Close'], window=20)
+    return data
