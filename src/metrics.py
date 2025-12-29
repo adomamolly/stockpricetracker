03e198406f063a251b86a04b422b26763ba91ae7
adwoa adoma <adomamolly@gmail.com>
2025-12-29 00:35:03 -0500

    Create metrics.py

---
+++ src/metrics.py
@@ -0,0 +1,11 @@
+
+
+def calculate_metrics(data):
+    last_close = data['Close'].iloc[-1]
+    prev_close = data['Close'].iloc[0]
+    change = last_close - prev_close
+    per_change = (change / prev_close) * 100
+    high = data['High'].max()
+    low = data['Low'].min()
+    vol = data['Volume'].sum()
+    return last_close, prev_close, change, per_change, high, low, vol
